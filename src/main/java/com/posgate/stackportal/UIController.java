fa912066f31dc250325e7b96013cbb3056c81d85
Philip Posgate <philip.posgate@gmail.com>
2022-12-10 03:12:45 -0500

    establish basic ui with thymeleaf and bootstrap

--- src/main/java/com/posgate/stackportal/UIController.java
+++ src/main/java/com/posgate/stackportal/UIController.java
@@ -3,15 +3,20 @@ package com.posgate.stackportal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UIController {
 
+    @GetMapping("/")
+	public String home(Model model) {
+        model.addAttribute("page", "home"); 
+		return "main";
+	}
+
     @GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
+	public String greeting(Model model) {
+        model.addAttribute("page", "greeting"); 
+		return "main";
 	}
 
 }
