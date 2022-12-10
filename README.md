022f23a6d79d4c5392a1e0d0564a8e2035b1542a
Philip Posgate <philip.posgate@gmail.com>
2022-12-10 01:38:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Stack Portal
+
+
+## BASIC USAGE
+
+Build and deploy the app with the following commands...
+
+```
+kubectl create ns local-stackportal-ns
+kubectl apply -k kubernetes/overlays/local
+```
---
+++ src/test/java/com/posgate/stackportal/StackPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posgate.stackportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
